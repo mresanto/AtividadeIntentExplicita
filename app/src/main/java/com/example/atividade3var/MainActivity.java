daf907e0126a2397132bef830ab6f993e8e7f108
xNyku <micaelrodrigues504@gmail.com>
2020-07-09 09:24:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/atividade3var/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.atividade3var;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
